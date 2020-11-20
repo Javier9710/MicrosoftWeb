a7ffb3175737903ed3ea2513e07d0bb675eeeccb
Javier9710 <39739533+Javier9710@users.noreply.github.com>
2020-11-20 00:23:49 -0500

    Update RouteConfig.cs

--- ejercicioWeb2.0/App_Start/RouteConfig.cs
+++ ejercicioWeb2.0/App_Start/RouteConfig.cs
@@ -7,7 +7,7 @@ using System.Web.Routing;
 
 namespace ejercicioWeb2._0
 {
-    public class RouteConfig
+    public static class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
